2e62587afe859ed3ddaf5ccc5eca28c20574d8af
Prashant Singh <prashantsingh5356.com>
2025-07-11 20:18:57 +0530

    added font style to logo and refactored nav component

--- app/(home)/_components/Navbar.tsx
+++ app/(home)/_components/Navbar.tsx
@@ -1,55 +1,24 @@
-"use client";
-
 import * as React from "react";
-import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
-
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-
 import Link from "next/link";
+import ThemeButton from "./ThemeButton";
 
-function ModeToggle() {
-  const { setTheme } = useTheme();
+import { Zen_Tokyo_Zoo } from "next/font/google";
 
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon">
-          <Sun className=" h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
-}
+const zenFont = Zen_Tokyo_Zoo({
+  weight: "400",
+});
 
 const Navbar = () => {
   return (
-    <div className="w-full h-22  fixed top-0 flex justify-center z-1000">
+    <div className="w-full h-22  fixed top-0 flex justify-center z-500">
       <div className="max-md:w-full  flex flex-col justify-center">
         <div className=" dark:text-zinc-400 text-zinc-600 rounded-xl px-8 w-[100%] lg:min-w-[58rem] md:max-w-[60rem] md:min-w-[50rem] backdrop-filter backdrop-blur-lg dark:bg-black/30 bg-white/30   flex  justify-between items-center min-h-[4rem]">
-          <div className="font-extrabold text-black dark:text-white">
-            <Link href="/">PS</Link>
+          <div className="font-extrabold text-black dark:text-white text-2xl">
+            <Link href="/" className={zenFont.className}>
+              PS
+            </Link>
           </div>
-          <ul className="font-medium flex justify-around w-[50%] items-center h-10  font-sans  ">
+          <ul className="font-medium flex justify-around w-[50%] items-center h-10 text-sm ">
             <li className=" dark:hover:text-white hover:text-black ">
               <Link href="/">Home</Link>
             </li>
@@ -63,7 +32,7 @@ const Navbar = () => {
               <Link href="/about">About</Link>
             </li>
             <li>
-              <ModeToggle />
+              <ThemeButton />
             </li>
           </ul>
         </div>
